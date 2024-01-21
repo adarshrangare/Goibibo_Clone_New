3847738889d49f3c69fd04d7530d8ee245e04b81
Adarsh Rangare <akrangare@gmail.com>
2024-01-22 01:36:58 +0530

    make minor adjustment in mobile responsive in nav

--- src/components/Header/MyTripNav.jsx
+++ src/components/Header/MyTripNav.jsx
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom'
 const MyTripNav = () => {
   
   return (
-    <NavLink to={"/mysupport/trips"} className='flex items-center shrink-0 max-[400px]:hidden ' >
+    <NavLink to={"/mysupport/trips"} className='flex items-center shrink-0 max-sm:flex-col max-sm:justify-center ' >
         <div className="logo-trip logo-nav w-9 h-9 mr-1"></div>
-        <div className="text mr-5">
+        <div className="text mr-5 max-sm:mr-0">
             <div className="font-semibold text-xs text-blue-600 ">
                 My Trip
             </div>
-            <div className="font-semibold text-sm text-slate-600">
+            <div className="font-semibold text-sm text-slate-600 max-sm:hidden">
                 Manage Booking
             </div>
         </div>
--- src/components/Header/ProfileNav.jsx
+++ src/components/Header/ProfileNav.jsx
@@ -5,7 +5,7 @@ const ProfileNav = () => {
   return (
     <div
       to={"/profile"}
-      className="border border-solid border-blue-500 rounded-lg truncate flex flex-auto items-center w-[172px] h-11 px-[10px] gap-2 m-3 cursor-pointer"
+      className="border border-solid border-blue-500 rounded-lg truncate flex flex-auto items-center w-[172px] h-11 px-[10px] gap-2 m-3 cursor-pointer max-sm:w-fit"
     >
       <span className="logo-profile inline-block w-7 h-7 logo-nav"></span>
       <span className="text-blue-500 font-medium text-xs text-ellipsis overflow-hidden">LOGIN/SIGNUP</span>
