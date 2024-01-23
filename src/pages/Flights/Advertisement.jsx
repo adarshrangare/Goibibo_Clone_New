8f3768a69d962315e117fbfe522c7a9894790b2c
Adarsh Rangare <akrangare@gmail.com>
2024-01-24 01:56:37 +0530

    ad image fix try 1

--- src/pages/Flights/Advertisement.jsx
+++ src/pages/Flights/Advertisement.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import image from '../../assets/advertisment.jpg'
 const Advertisement = () => {
   return (
-    <div className="advertisment mt-16 mb-10 w-full rounded-lg mx-auto overflow-hidden md:my-20 md:w-8/12 shadow"> 
-
+    <div className=" mt-16 mb-10 w-full rounded-lg mx-auto overflow-hidden md:my-20 md:w-8/12 shadow"> 
+        {/* // Ad */}
             <img src={image} className="w-full" alt="advertisment"/>
 
         </div>
