fc6432d34d2b1453730fb5e8cd6aeaf78daad2a3
Adarsh Rangare <akrangare@gmail.com>
2024-04-05 21:42:13 +0530

    vercel config3

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import AuthProvider from "./context/AuthProvider";
 import { FlightPassengerProvider } from "./context/index.js";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <FlightPassengerProvider>
-      <ToastContainer/>
+      <ToastContainer />
       <App />
     </FlightPassengerProvider>
   </AuthProvider>
