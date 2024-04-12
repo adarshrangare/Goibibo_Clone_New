f7b3f3de2358af45074bb59cf1827610deca3b36
Adarsh Rangare <akrangare@gmail.com>
2024-04-12 18:24:43 +0530

    added fevicons

--- src/components/SmallComponents/InputField/InputField.jsx
+++ src/components/SmallComponents/InputField/InputField.jsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import { useFlightPassanger } from "../../../context";
 import { useRef } from "react";
 
-
 const InputField = ({
   label,
   placeholder,
@@ -22,9 +21,7 @@ const InputField = ({
   const [airportsList, setAirportsList] = useState(null);
   const [showSuggetion, setShowSuggetion] = useState(false);
 
-  const {journeyDetails} = useFlightPassanger();
-
-
+  const { journeyDetails } = useFlightPassanger();
 
   useEffect(() => {
     fetchAirports().then((res) => {
@@ -71,16 +68,13 @@ const InputField = ({
         type={type}
         id={id}
         autoComplete="off"
-        value={inputValue}        
+        value={inputValue}
         className="w-full relative rounded-lg m-3 focus:outline-none  border-2 border-solid border-slate-200 hover:border-slate-500 focus:border-[rgb(34,118,227)] font-medium text-lg leading-7 text-[rgb(20, 24, 35)] py-3 px-4 md:py-5 md:px-4 "
         onChange={(e) => {
           // console.log(e.target.value);
           handleInput(e.target.value);
-          
         }}
       />
-
-      
 
       <label
         htmlFor={id}
