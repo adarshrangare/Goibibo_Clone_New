98725b36e393d3c1f66af8a95abc1ff9a68f6238
Adarsh Rangare <akrangare@gmail.com>
2024-04-16 12:38:38 +0530

    fixed button

--- src/components/SmallComponents/Buttons/SearchButton.jsx
+++ src/components/SmallComponents/Buttons/SearchButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchButton = ({type, className,handleSubmit}) => {
   return (
-    <button className={`${className} uppercase transition-all `}
+    <button className={`${className} uppercase transition-all text-nowrap `}
       onClick={handleSubmit}
     >
         SEARCH {type}
--- src/pages/FlightSearch/components/InputSection.jsx
+++ src/pages/FlightSearch/components/InputSection.jsx
@@ -1,5 +1,5 @@
 import { DatePicker } from "antd";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { SearchButton, SwapButton, TravellersCount } from "../../../components";
 import { useFlightPassanger } from "../../../context/FlightContext/FlightPassengerProvider";
 import "../style.css";
@@ -12,83 +12,79 @@ import { LoadingOutlined } from "@ant-design/icons";
 
 // import 'dayjs/locale/zh-cn';
 
-const InputSection = ({ journeyDetails, dispatchJourneyDetails,flightsList,
-  setFlightsList }) => {
+const InputSection = ({
+  journeyDetails,
+  dispatchJourneyDetails,
+  flightsList,
+  setFlightsList,
+}) => {
+  // console.log(journeyDetails);
 
-    // console.log(journeyDetails);
+  const { journeyDetails: details } = useFlightPassanger();
 
-    const {journeyDetails : details } = useFlightPassanger();
-
-    
   const { searchQuery } = useParams();
   // console.log({searchQuery});
 
-  const encodedString = searchQuery ?? '' ;
+  const encodedString = searchQuery ?? "";
 
-  const extractedEncodedPath = encodedString.replace('air-', '');
+  const extractedEncodedPath = encodedString.replace("air-", "");
   // console.log(extractedEncodedPath);
   // console.log(encoded);
   const decodedPath = atob(extractedEncodedPath);
-    // console.log(decodedPath);
+  // console.log(decodedPath);
   // console.log(decodedPath);
 
-  const [location, date, counts] =  decodedPath?.split("--");
+  const [location, date, counts] = decodedPath?.split("--");
   // console.log(location,date,counts);
-  
+
   const [source, dest] = location?.split("-");
 
   let [adult, child, infant] = counts?.split("-");
-  adult *=1;
-  child *=1;
-  infant *=1;
+  adult *= 1;
+  child *= 1;
+  infant *= 1;
 
-  console.log({adult,child,infant})
+  console.log({ adult, child, infant });
   const {
     source_location,
     destination_location,
-    oneway,
     travel_details,
     date_of_journey,
   } = journeyDetails;
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatchJourneyDetails({
       type: "set_travel_details_numbers",
       payload: { value: { adult, child, infant } },
     });
-
-  },[])
-  
+  }, []);
 
   const [inputSourceValue, setInputSourceValue] = useState(source);
   const [inputDestValue, setInputDestValue] = useState(dest);
   const [pageLoad, setPageLoad] = useState(false);
   // console.log(travel_details.numbers)
   const navigate = useNavigate();
-  function handleSearch(){
-      setPageLoad(true);
-      // console.log("hello")
+  function handleSearch() {
+    setPageLoad(true);
+    // console.log("hello")
 
-      // const {adult,child,infant} = travel_details.numbers;
-        // console.log({adult,child,infant},"inside handleSearch");
-    const encodedPath = btoa(`${source_location}-${destination_location}--${date_of_journey}--${travel_details.numbers.adult}-${travel_details.numbers.child}-${travel_details.numbers.infant}`)
+    // const {adult,child,infant} = travel_details.numbers;
+    // console.log({adult,child,infant},"inside handleSearch");
+    const encodedPath = btoa(
+      `${source_location}-${destination_location}--${date_of_journey}--${travel_details.numbers.adult}-${travel_details.numbers.child}-${travel_details.numbers.infant}`
+    );
 
-      // console.log(`${source_location}-${destination_location}--${date_of_journey}--${adult}-${child}-${infant}`);
+    // console.log(`${source_location}-${destination_location}--${date_of_journey}--${adult}-${child}-${infant}`);
 
-      setTimeout(()=>{
-        setPageLoad(false);
-        navigate(`/flight/air-${encodedPath}`)
-      },1000);
-      
+    setTimeout(() => {
+      setPageLoad(false);
+      navigate(`/flight/air-${encodedPath}`);
+    }, 1000);
   }
-
-  
 
   return (
     <div className="flex flex-col pt-1 pb-6 md:flex-row w-full lg:w-10/11 justify-center">
       <div className="flex overflow-x-hidden px-2 my-2 w-full md:w-fit lg:w-fit justify-center ">
-        
-
         <InputBox
           className=""
           placeholder="From"
@@ -97,7 +93,6 @@ const InputSection = ({ journeyDetails, dispatchJourneyDetails,flightsList,
           inputValue={inputSourceValue}
           setInputValue={setInputSourceValue}
           handleValue={(value) => {
-            
             dispatchJourneyDetails({
               type: "set_source_location",
               payload: { value },
@@ -130,13 +125,11 @@ const InputSection = ({ journeyDetails, dispatchJourneyDetails,flightsList,
           }}
         />
       </div>
-     
+
       <div className="flex my-2 mx-auto justify-center md:justify-start md:w-fit lg:w-fit gap-2 md:mx-4 ">
-        
         <DatePicker
           className="bg-blue-800 outline-none border-none font-medium h-8 hover:bg-blue-800 text-white px-2 py-2 "
           locale={locale}
-          
           format={"DD-MM-YYYY"}
           value={dayjs(date_of_journey)}
           onChange={(value) => {
@@ -172,9 +165,10 @@ const InputSection = ({ journeyDetails, dispatchJourneyDetails,flightsList,
         type={"UPDATE"}
         onClick={handleSearch}
         className="px-8 py-2 mt-1 bg-orange-500 w-fit mx-auto rounded-full font-medium text-base text-white hover:bg-orange-600 outline-none focus:outline-none absolute -bottom-6 left-[50%] translate-x-[-50%]  uppercase transition-all"
-      > {pageLoad? <LoadingOutlined/> :"UPDATE SEARCH"}</button>
-
-
+      >
+        {" "}
+        {pageLoad ? <LoadingOutlined /> : "UPDATE SEARCH"}
+      </button>
     </div>
   );
 };
